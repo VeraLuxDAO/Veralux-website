79fbef2c34fc28fcedd0fe618233fdf3af30273f
Oyase-shinobi <donjosy305@gmail.com>
2025-09-16 11:25:34 +0900

    modify: updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-  import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import Navbar from "./Pages/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/About/Footer";
